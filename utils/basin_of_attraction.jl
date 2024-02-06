799c4871d4972d3e5dd29798555bb07ee86e9e2c
Kevin Tracy <kevt.laser@gmail.com>
2024-02-05 16:04:52 -0800

    converted plot to degrees

--- utils/basin_of_attraction.jl
+++ utils/basin_of_attraction.jl
@@ -1,6 +1,6 @@
 function plot_basin_of_attraction(initial_conditions, convergence_list, ps, thetas)
     function nearest_neighbor(x, y)
-        distances = [norm(xic .- [x,y,0,0]) for xic in initial_conditions]
+        distances = [norm([xic[1], rad2deg(xic[2]),0, 0] .- [x,y,0,0]) for xic in initial_conditions]
         min_distance, nearest_index = findmin(distances)
         return convergence_list[nearest_index]
     end
@@ -12,11 +12,11 @@ function plot_basin_of_attraction(initial_conditions, convergence_list, ps, thet
         else
             color = :red
         end
-        plot!([initial_condition[1]], [(initial_condition[2])],
+        plot!([initial_condition[1]], [rad2deg(initial_condition[2])],
               seriestype=:scatter, color=color, label = false)
     end
         
     plot!([],[],seriestype=:scatter, color=:red, label = "failure")
     plot!([],[],seriestype=:scatter, color=:green, label = "success")
-    display(plot!(xlabel = "p_0 (meters)", ylabel = "θ_0 (rad)", legend = true))
+    display(plot!(xlabel = "p_0 (meters)", ylabel = "θ_0 (degrees)", legend = true))
 end
